f1f27b447fe1033156b9c3b6aec0b180398a697a
nbabcock <nathan.r.babcock@gmail.com>
2021-01-25 13:06:06 -0600

    Place houses

--- src/App.tsx
+++ src/App.tsx
@@ -21,9 +21,18 @@ const toGrid = ({x, y}: {x: number, y: number}) => ({
   y: Math.ceil(y / GRID_SIZE) - 1,
 });
 
-export default class App extends React.Component<{}, { ghostBuilding: Building | null}> {
-  currentbuildMode: string | undefined;
+export default class App extends React.Component<{}, { ghostBuilding: Building | null, buildings: Building[]}> {
+  currentbuildMode: string | null = null;
   test: string = 'hello';
+
+  onClick(event: MouseEvent) {
+    if (!this.state.ghostBuilding) { return; }
+    this.currentbuildMode = null;
+    this.setState({
+      buildings: [...this.state.buildings, this.state.ghostBuilding],
+      ghostBuilding: null,
+    });
+  }
 
   onMousemove(event: MouseEvent) {
     if (this.currentbuildMode === undefined) { return; }
@@ -36,24 +45,38 @@ export default class App extends React.Component<{}, { ghostBuilding: Building |
 
   componentDidMount() {
     window.addEventListener('mousemove', this.onMousemove.bind(this));
+    window.addEventListener('click', this.onClick.bind(this));
   }
 
   componentWillUnmount() {
     window.removeEventListener('mousemove', this.onMousemove.bind(this));
+    window.removeEventListener('click', this.onMousemove.bind(this));
   }
 
   setBuildMode(buildingType: string, event: React.MouseEvent) {
+    // Toggle build mode back off
+    if (this.currentbuildMode !== null) {
+      this.currentbuildMode = null;
+      this.setState({ghostBuilding: null})
+      event.stopPropagation();
+      return;
+    }
+
     this.currentbuildMode = buildingType;
     const gridCoords = toGrid({ x:event.clientX, y: event.clientY });
     this.setState({ghostBuilding: new Building(gridCoords.x, gridCoords.y, buildingType)});
     console.log(this.state.ghostBuilding);
     console.log(!!this.state.ghostBuilding);
+    event.stopPropagation();
   }
 
   render() {
     return (
       <div>
-        {this.state.ghostBuilding && (<House className="ghost building" style={{ left: this.state.ghostBuilding.x * GRID_SIZE, top: this.state.ghostBuilding.y * GRID_SIZE }}/>)}
+        <div className="buildings">
+          {this.state.buildings.map(elem => (<House className="building" style={{ left: elem.x * GRID_SIZE, top: elem.y * GRID_SIZE }}/>))}
+          {this.state.ghostBuilding && (<House className="ghost building" style={{ left: this.state.ghostBuilding.x * GRID_SIZE, top: this.state.ghostBuilding.y * GRID_SIZE }}/>)}
+        </div>
         <div className="build-ui">
           <strong>Build</strong>
           <button onClick={(event) => this.setBuildMode('house', event)}><House/></button>
@@ -66,6 +89,7 @@ export default class App extends React.Component<{}, { ghostBuilding: Building |
     super(props);
     this.state = {
       ghostBuilding: null,
+      buildings: [],
     }
   }
 }
